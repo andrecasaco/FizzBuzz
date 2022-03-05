39c3628c2d600f0446780f2db5f4a40a3e8face0
Andre Casaco <andrecasaco7@gmail.com>
2022-03-05 23:05:17 +0000

    Try catch added

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,11 +1,16 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class FizzBuzz {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int input = 0;
         System.out.print("Number: ");
-        int input = sc.nextInt();
+        try {
+            input = sc.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Not a number");
+        }
 
         if (input % 3 == 0 && input % 5 == 0) //divisible by 3 or 5
             System.out.println("FizzBuzz");
@@ -14,6 +19,6 @@ public class FizzBuzz {
         else if (input % 5 == 0)
             System.out.println("Buzz"); // divisible by 3
         else
-            System.out.println(input + "Not divisible by 3 or 5");
+            System.out.println(input + " Not divisible by 3 or 5");
     }
 }
