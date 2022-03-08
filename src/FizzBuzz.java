84d5690996bbc7238d7af0025be130f5245f5361
Andre Casaco <andrecasaco7@gmail.com>
2022-03-08 10:18:52 +0000

    While loop fixed

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,23 +1,35 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
+/*
+“Fizz” if an integer is divisible by 3,
+“Buzz” if an integer is divisible by 5,
+and “FizzBuzz” if an integer is divisible by both 3 and 5.
+ */
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int input = 0;
+        boolean success = false;
+
         System.out.print("Number: ");
-        try {
-            input = sc.nextInt();
-        } catch (InputMismatchException e) {
-            System.out.println("Not a number");
+        while (!success) {
+            try { // try to insert a number
+                Scanner sc = new Scanner(System.in);
+                input = sc.nextInt();
+                success = true;
+            } catch (InputMismatchException e) { /* if the input is not a
+            number catch the error and ask the user to insert the number again*/
+                System.out.println("Not a number");
+                System.out.println("Re-enter Number");
+            }
         }
 
-        if (input % 3 == 0 && input % 5 == 0) //divisible by 3 or 5
+        if (input % 3 == 0 && input % 5 == 0) // check if number is divisible by 3 or 5
             System.out.println("FizzBuzz");
         else if (input % 3 == 0)
-            System.out.println("Fizz"); //divisible by 5
+            System.out.println("Fizz"); //check if number is divisible by 5
         else if (input % 5 == 0)
-            System.out.println("Buzz"); // divisible by 3
+            System.out.println("Buzz"); //check if number is divisible by 3
         else
             System.out.println(input + " Not divisible by 3 or 5");
     }
